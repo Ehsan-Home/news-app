5ff8e87d3e226b5586017ca7cd305bd2d7192e11
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-21 13:46:59 -0700

    add more styling to app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
 // All GLOBAL setup is done here
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
+import styles from "@/styles/App.module.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { Container } from "react-bootstrap";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
     </div>
   );
 }
