a34275cd43c43ec95de3df21bcbcb4be37122aa9
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-21 13:38:27 -0700

    unnecessary styling removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// All GLOBAL setup is done here
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title>News app</title>
+        <meta name="description" content="News app by Next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
