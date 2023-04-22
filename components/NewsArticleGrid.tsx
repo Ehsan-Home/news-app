e303a463d530eaf55f4b98a225c976477cc81117
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-21 18:46:04 -0700

    NewsGrid component developed

--- components/NewsArticleGrid.tsx
+++ components/NewsArticleGrid.tsx
@@ -1,5 +1,17 @@
-const NewsArticleGrid = () => {
-  return <div>NewsArticleGrid</div>;
+import { NewsArticles } from "@/models/NewsArticles";
+import { Col, Row } from "react-bootstrap";
+import NewArticleCard from "./NewsArticleCard";
+
+const NewsArticleGrid = ({ articles }: NewsArticles) => {
+  return (
+    <Row className="g-4">
+      {articles.map((article) => (
+        <Col sm={12} md={6} lg={4} key={article.url}>
+          <NewArticleCard article={article} />
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default NewsArticleGrid;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,13 @@ import NewsArticleGrid from "@/components/NewsArticleGrid";
 import { NewsArticles, Article } from "@/models/NewsArticles";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { Alert } from "react-bootstrap";
 
 export const getServerSideProps: GetServerSideProps<
   NewsArticles
 > = async () => {
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=ca&apiKey=${process.env.NEWS_API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
   );
   const responseJSON: NewsArticles = await response.json();
   return {
@@ -23,8 +24,12 @@ export default function BreakingNewsPage({ articles }: NewsArticles) {
         <title key="title">Breaking News!</title>
       </Head>
       <h1>Breaking News</h1>
-      <NewsArticleGrid />
-      <NewArticleCard article={articles[0]} />
+      <Alert>
+        This page uses <strong>getServerSideProps</strong> to fetch data
+        server-side on every request. This allows search engines to crawl the
+        page content and <strong>improves SEO</strong>.
+      </Alert>
+      <NewsArticleGrid articles={articles} />
     </>
   );
 }
