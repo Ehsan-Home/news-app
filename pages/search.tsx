09ba27168a51eb957f6649447330a9a5f5063c6b
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-28 11:37:28 -0700

    search UI added

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,7 +1,30 @@
+import { FormEvent } from "react";
+import { Button, Form } from "react-bootstrap";
+
 const Search = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const searchQueryInput = document.getElementById(
+      "search-query"
+    ) as HTMLInputElement;
+    const searchQuery = searchQueryInput.value;
+    console.log(searchQuery);
+  };
+
   return (
     <>
-      <div>Search page</div>
+      <h3>Search</h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Search query</Form.Label>
+          <Form.Control
+            id="search-query"
+            name="search-query"
+            placeholder="E.g. Sports, politics,..."
+          ></Form.Control>
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </Form>
     </>
   );
 };
