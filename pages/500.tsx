d1bc812bf80ed02856cecfad26cd7065fe6b9db9
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-28 18:00:15 -0700

    500 page added

---
+++ pages/500.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <>
+      <h3>Error!</h3>
+      <p>Something went wrong. Please try again or contact support.</p>
+    </>
+  );
+};
+
+export default ErrorPage;
