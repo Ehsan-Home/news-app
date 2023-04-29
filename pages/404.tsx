22bc57f17e9a4218de91455ed814d030b81ed582
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-04-28 17:58:55 -0700

    404 page added

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <>
+      <h3>Not Found.</h3>
+      <p>The page your are looking for does not exist :(</p>
+    </>
+  );
+};
+
+export default NotFoundPage;
