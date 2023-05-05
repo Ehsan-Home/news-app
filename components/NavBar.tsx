2031c0188346a30e38fdff32643c3e0d43c64640
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-05-04 17:46:03 -0700

    navbar components name fixed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,25 +1,35 @@
+import React, { Component } from "react";
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
+  const categories = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+  ];
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">News App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+            <Nav.Link href="#home">Breaking News</Nav.Link>
+            <Nav.Link href="#link">Search</Nav.Link>
+            <NavDropdown title="Categories" id="basic-nav-dropdown">
+              {categories.map((category) => {
+                return (
+                  <React.Fragment key={category}>
+                    <NavDropdown.Item href="#action/3.1">
+                      {category}
+                    </NavDropdown.Item>
+                  </React.Fragment>
+                );
+              })}
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
