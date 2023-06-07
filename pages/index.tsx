843b6003de9a0c9dff50a36fbbe8d2da30e8c2c3
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-06 22:01:15 -0700

    basic structure of homepage designed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,57 @@
-import NewArticleCard from "@/components/NewsArticleCard";
-import NewsArticleGrid from "@/components/NewsArticleGrid";
-import { NewsArticles, Article } from "@/models/NewsArticles";
-import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { Alert } from "react-bootstrap";
 
-export const getServerSideProps: GetServerSideProps<
-  NewsArticles
-> = async () => {
-  const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
-  );
-  const responseJSON: NewsArticles = await response.json();
-  return {
-    props: { articles: responseJSON.articles },
-  };
-};
-
-export default function BreakingNewsPage({ articles }: NewsArticles) {
+export default function BreakingNewsPage() {
   return (
     <>
-      <Head>
-        <title key="title">Breaking News!</title>
-      </Head>
-      <h1>Breaking News</h1>
-      <Alert>
-        This page uses <strong>getServerSideProps</strong> to fetch data
-        server-side on every request. This allows search engines to crawl the
-        page content and <strong>improves SEO</strong>.
-      </Alert>
-      <NewsArticleGrid articles={articles} />
+      {/* <img
+        src={img}
+        class="mx-auto d-block mt-3"
+        width={150}
+        height={150}
+        alt="GitHub logo"
+      ></img> */}
+      <div className="text-center mt-3">
+        <h1 className="display-1" style={{ color: "#0d6efd" }}>
+          InfoSphere
+        </h1>
+      </div>
+      <p className="lead text-center">
+        📰 Empowering Minds with Real-Time News
+      </p>
+      <p className="text-center text-secondary">
+        Introducing InfoSphere, an innovative news web app designed to keep you
+        informed and engaged in a rapidly evolving world. InfoSphere leverages
+        cutting-edge technology to deliver real-time news updates, ensuring you
+        never miss a beat. With its sleek and intuitive interface, InfoSphere
+        provides a seamless browsing experience, allowing you to effortlessly
+        navigate through a wide range of topics, from politics and business to
+        science and entertainment. Whether you're a news enthusiast, a
+        professional seeking the latest trends, or simply looking to stay
+        informed, InfoSphere is your go-to destination for staying ahead in an
+        interconnected global society. Embrace the power of knowledge and embark
+        on a transformative journey with InfoSphere.
+      </p>
+      <div className="d-flex justify-content-center">
+        <div className="p-2">
+          <a
+            role="button"
+            href="/breakingnews"
+            className="btn btn-lg btn-primary"
+          >
+            Breaking News
+          </a>
+        </div>
+        <div className="p-2">
+          <a role="button" href="/search" className="btn btn-lg btn-dark">
+            Search
+          </a>
+        </div>
+      </div>
+      <div className="text-secondary text-center my-4">
+        {new Date().getFullYear()} @
+        <a href="https://ehsanghasaei.netlify.app/">Ehsan Ghasaei</a>
+      </div>
     </>
   );
 }
