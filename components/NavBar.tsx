a999fc1794f38173f12b994f06c7a7f0ff8e078d
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-07 11:05:23 -0700

    navbar brand now acts a a next link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -15,7 +15,9 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">InfoSphere</Navbar.Brand>
+        <Navbar.Brand as={Link} href="/">
+          InfoSphere
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
