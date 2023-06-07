35ed1d7227d8ca67c7a88d2d5a86df5eacf6ed5c
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-07 08:51:46 -0700

    navbar brand href fixed

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">News App</Navbar.Brand>
+        <Navbar.Brand href="/">InfoSphere</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
