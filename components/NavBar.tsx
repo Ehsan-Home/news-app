56d21e1ea138e76d968043951de9371abf58f64a
ehsanghasaei <ehsan.ghasaei@gmail.com>
2023-06-06 21:14:33 -0700

    breaking news page added

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -19,7 +19,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} href="/">
+            <Nav.Link as={Link} href="/breakingnews">
               Breaking News
             </Nav.Link>
             <Nav.Link as={Link} href="/search">
---
+++ pages/breakingnews.tsx
@@ -0,0 +1,35 @@
+import NewArticleCard from "@/components/NewsArticleCard";
+import NewsArticleGrid from "@/components/NewsArticleGrid";
+import { NewsArticles, Article } from "@/models/NewsArticles";
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import { Alert } from "react-bootstrap";
+
+export const getServerSideProps: GetServerSideProps<
+  NewsArticles
+> = async () => {
+  const response = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`
+  );
+  const responseJSON: NewsArticles = await response.json();
+  return {
+    props: { articles: responseJSON.articles },
+  };
+};
+
+export default function BreakingNewsPage({ articles }: NewsArticles) {
+  return (
+    <>
+      <Head>
+        <title key="title">Breaking News!</title>
+      </Head>
+      <h1>Breaking News</h1>
+      <Alert>
+        This page uses <strong>getServerSideProps</strong> to fetch data
+        server-side on every request. This allows search engines to crawl the
+        page content and <strong>improves SEO</strong>.
+      </Alert>
+      <NewsArticleGrid articles={articles} />
+    </>
+  );
+}
